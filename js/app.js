f4828f94e2a70779ca6035c4ec4ce96396c55096
andry16-10-91@hotmail.it <andry16-10-91@hotmail.it>
2022-03-04 14:46:12 +0100

    push email

--- js/app.js
+++ js/app.js
@@ -8,11 +8,18 @@ const app = new Vue ({
 
     created() {
 
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then( (response) => {
-            console.log(response);
-        });
+        for (let i = 0; i < 10; i++) {
 
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then( (resp) => {
+
+                const email = resp.data.response;
+
+                this.listEmail.push(email);
+                //console.log(email);
+            });
+            
+        }
     },
 })
 console.log(app);
